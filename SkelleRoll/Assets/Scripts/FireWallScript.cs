a898750fc1bc01644f8fa9ad121d173cad4c6226
thewildvegetable <whichazelnj13@aol.com>
2016-11-16 16:11:27 -0500

    Add base gameManager

--- SkelleRoll/Assets/Scripts/FireWallScript.cs
+++ SkelleRoll/Assets/Scripts/FireWallScript.cs
@@ -4,18 +4,17 @@ using System.Collections;
 public class FireWallScript : MonoBehaviour {
 
     public GameObject fireWall;
-    private ArrayList handles = new ArrayList();   //all the handles in the scene
+    public GameObject gameManager;
     public GameObject target;      //target to seek/path follow
     private float distance = float.MaxValue;
     private Vector3 seekingForce = Vector3.zero;
     private Vector3 steeringForce = Vector3.zero;
-    private float seekWeight = 1.3f;
+    public float seekWeight = 0.8f;
+    private int listPos = 0;        //position in the handle list
 
 	// Use this for initialization
 	void Start () {
-        handles = new ArrayList();
-        handles.AddRange(GameObject.FindGameObjectsWithTag("frontHandle"));
-        handles.AddRange(GameObject.FindGameObjectsWithTag("rearHandle"));
+
 	}
 	
 	// Update is called once per frame
@@ -34,33 +33,8 @@ public class FireWallScript : MonoBehaviour {
             //check distance between fireWall and target
             if (distance <= 1)
             {
-                //find all handles, find the closest handle that isnt this one
-                handles = new ArrayList();
-                handles.AddRange(GameObject.FindGameObjectsWithTag("frontHandle"));
-                handles.AddRange(GameObject.FindGameObjectsWithTag("rearHandle"));
-                if (handles.Count > 0)
-                {
-                    GameObject nextTarget = target;
-                    float shortestDist = float.MaxValue;
-                    float tempDist = float.MaxValue;
-                    foreach (GameObject temp in handles)
-                    {
-                        //get distance to next handle in the array
-                        tempDist = (temp.transform.position - fireWall.transform.position).magnitude;
-
-                        //check if tempDistance is > 1 (ie not the distance to current target) and closest handle
-                        if (tempDist > 1 && tempDist < shortestDist)
-                        {
-                            shortestDist = tempDist;
-                            nextTarget = temp;
-                        }
-                    }
-                    target = nextTarget;
-                }
-                else
-                {
-                    target = null;
-                }
+                listPos++;
+                target = gameManager.GetComponent<GameManagerScript>().handles[listPos];
             }
 
             //make fireWall look at target
