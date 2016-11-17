02f5859aaa10731b447b90ea8b6578c070719d32
rochelle Cohn (RIT Student) <rc7604@ad.rit.edu>
2016-11-17 15:45:20 -0500

    Working on powerups. There will be arrays for holding data about the powerup so the size and amount of powerups can be changed. Also, so that multiple can be active at the same time. Might change the array to lists.

--- SkelleRoll/Assets/Scripts/SkullpowerUps.cs
+++ SkelleRoll/Assets/Scripts/SkullpowerUps.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class SkullpowerUps : MonoBehaviour {
 
-	// Use this for initialization
-    private bool
-	void Start () {
-	
+    // Use this for initialization
+    private float currInvinsTime;//current time invinsible
+
+    private bool[] powerupActive;//a bunch of bools to tell which powerup is active
+    public float[] powerupTime;//tells how long the powerup corresponding is active for
+    private float[] powerCurrTime;//tells how long the powerup has been active for
+    /// <summary>
+    /// In array index:
+    /// 0 is invinsibility
+    /// </summary>
+
+    void Start () {
+        powerupActive[0] = false;
 	}
 	
 	// Update is called once per frame
