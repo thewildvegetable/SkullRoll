f1dbd414b55e419932eeb2fa18a917293f492ccd
Sulfur <rc7604@ad.rit.edu>
2016-11-17 12:16:47 -0500

    Working on scripts for powerups

---
+++ SkelleRoll/Assets/Scripts/SkullpowerUps.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkullpowerUps : MonoBehaviour {
+
+	// Use this for initialization
+    private bool
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
