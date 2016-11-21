ac29700726e0feb46106f3512579c41f979d5dbf
JamesM <jpm5399@g.rit.edu>
2016-11-20 19:34:50 -0500

    Added Ballcontrolling script

---
+++ SkelleRoll/Assets/Scripts/BallControls.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControls : MonoBehaviour {
+
+    public float speed;
+    public float jumpCooldown;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float xspeed = Input.GetAxis("Horizontal");
+        float zspeed = Input.GetAxis("Vertical");
+
+        float yspeed = 0f;
+
+        if(Input.GetKey(KeyCode.Space) && jumpCooldown > 150)
+        {
+            yspeed = 30;
+            jumpCooldown = 0;
+        }
+
+        Vector3 force = new Vector3(xspeed, yspeed, zspeed);
+
+        rb.AddForce(force * speed);
+
+        jumpCooldown += 1;
+    }
+}
+
