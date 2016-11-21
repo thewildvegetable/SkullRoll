768f8fac2ad03e30834b7009c8e304c05fdbdda4
Sulfur <rc7604@g.rit.edu>
2016-11-21 01:09:02 -0500

    Working on power ups. Also trying to get trigger for powerups

--- SkelleRoll/Assets/Scripts/InvinciblePick.cs
+++ SkelleRoll/Assets/Scripts/InvinciblePick.cs
@@ -12,4 +12,9 @@ public class InvinciblePick : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider ply)
+    {
+
+    }
 }
--- SkelleRoll/Assets/Scripts/SkullpowerUps.cs
+++ SkelleRoll/Assets/Scripts/SkullpowerUps.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SkullpowerUps : MonoBehaviour {
+public class SkullpowerUps : MonoBehaviour
+{
 
 	// Use this for initialization
-    private bool
+    private float currInvinsTime;//current time invinsible
+ 
+    public bool[] powerupActive;//a bunch of bools to tell which powerup is active
+     public float[] powerupTime;//tells how long the powerup corresponding is active for
+     private float[] powerCurrTime;//tells how long the powerup has been active for
+     /// <summary>
+    /// In array index:
+    /// 0 is invinsibility
+  /// </summary>
 	void Start () {
-	
-	}
+        powerupActive[0] = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
