6163c9e6f71efce1e6e1213266707efa0fba996d
thewildvegetable <whichazelnj13@aol.com>
2016-12-01 11:16:22 -0500

    Floor script finds firewall

--- SkelleRoll/Assets/Scripts/FloorScript.cs
+++ SkelleRoll/Assets/Scripts/FloorScript.cs
@@ -5,18 +5,18 @@ using System.Collections.Generic;
 public class FloorScript : MonoBehaviour {
 
     public List<GameObject> spawners = new List<GameObject>();
-    public GameObject skull;
+    GameObject firewall;
 
 	// Use this for initialization
 	void Start () {
-
+        firewall = GameObject.FindGameObjectWithTag("FireWall");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         //if time passed a certain point then delete this
-        if (skull.transform.position.z >= this.transform.position.z + 30)
+        if (firewall.transform.position.z >= this.transform.position.z + 30)
         {
             Destroy(this);
         }
