57f54584f8188eef9448eecfb19982ce26106fee
JamesM <jpm5399@g.rit.edu>
2016-12-04 19:32:46 -0500

    "Enemy Thing"

---
+++ SkelleRoll/Assets/Scripts/RotatingEnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingEnemyScript : MonoBehaviour
+{
+
+    public int spinSpeedx;
+    public int spinSpeedy;
+    public int spinSpeedz;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotating on X
+        transform.Rotate(Vector3.right * spinSpeedx);
+
+        //Rotating on Y
+        transform.Rotate(Vector3.up * spinSpeedy);
+
+        //Rotating on Z
+        transform.Rotate(Vector3.forward * spinSpeedz);
+    }
+}
